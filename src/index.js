c793753203e7dcea5d4637d97f760cbe1d3d25d1
heather-tech <heather.k.tech@gmail.com>
2023-04-05 09:24:35 -0400

    multiple changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  //fetch books
-  fetch('http://localhost:3000/books')
-    .then(response => response.json())
-    // once books are fetched-render a card for each book
-   .then(data => data.forEach(renderBooks))
-});
-
 function renderBooks(books){
   const bookImg = document.getElementById("book-collection")
   const eachBook = document.createElement("img")
@@ -14,21 +5,36 @@ function renderBooks(books){
   bookImg.append(eachBook)
 }
 
+//gloabal variables
+const cardContainer = document.querySelector('#card-container');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  //fetch books
+  fetch('http://localhost:3000/books')
+    .then(response => response.json())
+    // once books are fetched-render a card for each book
+   .then(data => {
+    cardContainer.innerHTML = '';
+    renderCard(data)
+  })
+  .catch(error => console.log('Error:', error));
+});
+
+
+function renderCard(data){
+  data.results.map(book => {
+    const cardDiv = document.createElement('div');
+    const cardTitle = document.createElement('p');
+    const cardImg = document.createElement('img');
+
+    cardTitle.textContent = book.title;
+    cardDiv.classList.add('cardDefault');
+    cardImg.classList.add('cardImg');
+
+    cardContainer.appendChild(cardDiv);
+    cardDiv.append(cardTitle, cardImg);
+  });
+}  
 // functional drop down list 
-
-// function myCollection() {
-//   let sHBooks = [
-//     {
-//       "title" : "The Body Keeps Score"
-//     }
-//   ]
-// }
-
-
-
-
-
-
-
-
-
